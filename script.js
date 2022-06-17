3ba9aff07afd912207394c6f72451802e92d71e8
xwexerx <xwexerx@gmail.com>
2022-06-17 09:23:32 +0200

    Finished game function and score system

--- script.js
+++ script.js
@@ -4,31 +4,33 @@ function playRound(playerSelection, computerSelection) {
     let computerChoice = computerSelection.toLowerCase();
 
     if (playerChoice == computerChoice) {
+        computerPoints++;
+        playerPoints++;
         return "Draw"
     }
     else if (playerChoice == "rock" && computerChoice == "paper") {
         computerPoints++;
-        return "You have lost!"
+        return "You have lost! Rock gets beaten by paper."
     }
     else if (playerChoice == "rock" && computerChoice == "scissors") {
         playerPoints++;
-        return "You have won!"
+        return "You have won! Rock beats paper."
     }
     else if (playerChoice == "paper" && computerChoice == "rock") {
         playerPoints++;
-        return "You have won!"
+        return "You have won! Paper beats rock."
     }
     else if (playerChoice == "paper" && computerChoice == "scissors") {
         computerPoints++;
-        return "You have lost!"
+        return "You have lost! Paper gets beaten by scissors."
     }
     else if (playerChoice == "scissors" && computerChoice == "rock") {
         computerPoints++;
-        return "You have lost!"
+        return "You have lost! Scissors get beaten by rock."
     }
     else if (playerChoice == "scissors" && computerChoice == "paper") {
         playerPoints++;
-        return "You have won!"
+        return "You have won! Scissors beat paper."
     }
 }
 
@@ -58,8 +60,12 @@ let computerPoints = 0;
 
 function game() {
     for (let i = 0; i < 5; i++) {
+        playerSelection = prompt("What's your choice?");
+        computerSelection = computerPlay();
         console.log(playRound(playerSelection, computerSelection));
     }
+    console.log(`Your score = ${playerPoints}
+The Computer's score = ${computerPoints}`)
     if (playerPoints > computerPoints) {
         return "You have won the 5 round game!"
     }
