21d49523791661268dd0dcbb014b6425474095f2
xwexerx <xwexerx@gmail.com>
2022-06-30 15:29:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 //This function takes two choices and based on these choices, returns a winner.
 function playRound(playerSelection, computerSelection) {
 
+    // Get a new computer choice each time the function is executed.
+    computerSelection = computerPlay();
+
     //Transform the choices into lower case for ease of use in the code.
     let playerChoice = playerSelection.toLowerCase();
     let computerChoice = computerSelection.toLowerCase();
@@ -10,30 +13,44 @@ function playRound(playerSelection, computerSelection) {
     if (playerChoice == computerChoice) {
         computerPoints++;
         playerPoints++;
+        updateScore();
+        checkForWinner();
         return "Draw"
     }
     else if (playerChoice == "rock" && computerChoice == "paper") {
         computerPoints++;
+        updateScore();
+        checkForWinner();
         return "You have lost! Rock gets beaten by paper."
     }
     else if (playerChoice == "rock" && computerChoice == "scissors") {
         playerPoints++;
+        updateScore();
+        checkForWinner();
         return "You have won! Rock beats paper."
     }
     else if (playerChoice == "paper" && computerChoice == "rock") {
         playerPoints++;
+        updateScore();
+        checkForWinner();
         return "You have won! Paper beats rock."
     }
     else if (playerChoice == "paper" && computerChoice == "scissors") {
         computerPoints++;
+        updateScore();
+        checkForWinner();
         return "You have lost! Paper gets beaten by scissors."
     }
     else if (playerChoice == "scissors" && computerChoice == "rock") {
         computerPoints++;
+        updateScore();
+        checkForWinner();
         return "You have lost! Scissors get beaten by rock."
     }
     else if (playerChoice == "scissors" && computerChoice == "paper") {
         playerPoints++;
+        updateScore();
+        checkForWinner();
         return "You have won! Scissors beat paper."
     }
 }
@@ -63,9 +80,29 @@ let computerSelection = computerPlay();
 let playerPoints = 0;
 let computerPoints = 0;
 
+
+function updateScore() {
+    resultsElement.innerText = `${playerPoints} - ${computerPoints}`;
+}
+
+function checkForWinner() {
+    if ((playerPoints || computerPoints) == 5 && playerPoints > computerPoints) {
+        resultsElement.innerText = `${playerPoints} - ${computerPoints} You have won the 5 round game!`;
+        replayElement.innerText = "Reload the page to play again.";
+    }
+    else if ((computerPoints || playerPoints) == 5 && playerPoints < computerPoints) {
+        resultsElement.innerText = `${playerPoints} - ${computerPoints} You have lost the 5 round game!`;
+        replayElement.innerText = "Reload the page to play again.";
+    }
+    else if (playerPoints == 5 && computerPoints == 5){
+        resultsElement.innerText = `${playerPoints} - ${computerPoints} It's a draw!`;
+        replayElement.innerText = "Reload the page to play again.";
+    }
+}
+
 //This is the main function of the game. It plays out a 5-round game,
 //and at the end decides who's the winner based on the amounts of points they have.
-function game() {
+/*function game() {
     for (let i = 0; i < 5; i++) {
         playerSelection = prompt("What's your choice?");
         computerSelection = computerPlay();
@@ -82,6 +119,39 @@ The Computer's score = ${computerPoints}`)
     else {
         return "It's a draw!"
     }
-}
+}*/
 
-console.log(game());
+// Add three buttons to the HTMl
+const rockElement = document.createElement("button");
+rockElement.style.cssText = 'width: 100px;height:30px;';
+rockElement.innerText = "Rock";
+rockElement.addEventListener("click", () => console.log(playRound("rock", computerSelection)));
+document.body.appendChild(rockElement);
+
+const paperElement = document.createElement("button");
+paperElement.style.cssText = 'width: 100px;height:30px;';
+paperElement.innerText = "Paper";
+paperElement.addEventListener("click", () => console.log(playRound("paper", computerSelection)));
+document.body.appendChild(paperElement);
+
+const scissorsElement = document.createElement("button");
+scissorsElement.style.cssText = 'width: 100px;height:30px;';
+scissorsElement.innerText = "Scissors";
+scissorsElement.addEventListener("click", () => console.log(playRound("scissors", computerSelection)));
+document.body.appendChild(scissorsElement);
+
+// Add a div for displaying results
+const resultsElement = document.createElement("div");
+resultsElement.innerText = `${playerPoints} - ${computerPoints}`;
+document.body.appendChild(resultsElement);
+
+
+// Add a text element that get it's text updated after the game has ended
+
+const replayElement = document.createElement("p");
+replayElement.innerText = "";
+document.body.appendChild(replayElement);
+
+
+
+
